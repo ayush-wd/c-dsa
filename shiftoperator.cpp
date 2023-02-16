b3c9d0d16a54651367db00f5e35cb5070dbed741
Ayush yadav <20ayush07yadav03@gmail.com>
2023-02-17 01:54:12 +0530

    jhb

---
+++ shiftoperator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    a=4;
+    b=6;
+    // cout<<"a&b "<< (a&b) <<endl;
+    // cout<<"a|b "<< (a|b) <<endl;
+    // cout<<"~a " << ~a <<endl;
+    // cout<<"a^b "<< (a^b) <<endl;
+    cout<< (17>>1) <<endl;
+    cout<< (19<<1) <<endl;   
+}
